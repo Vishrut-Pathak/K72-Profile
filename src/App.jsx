d88e2d7be45c2b897b0686c39c44a2b7ac068f80
Vishrut-Pathak <vishrutpathak.1405@gmail.com>
2026-07-11 22:28:02 +0530

    Added new folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Agence from './Pages/Agence'
+import Projects from './Pages/Projects'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/agence' element={<Agence />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
+      </Routes>
+
+
+    </div>
   )
 }
 
