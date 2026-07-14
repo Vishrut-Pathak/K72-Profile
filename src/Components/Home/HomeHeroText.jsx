330059d32eff601b00714bdf93fcbc8ceb9c33f7
Vishrut-Pathak <vishrutpathak.1405@gmail.com>
2026-07-15 00:18:26 +0530

    added hero Text

--- src/Components/Home/HomeHeroText.jsx
+++ src/Components/Home/HomeHeroText.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
-    <div className='font-[font1]'>
-      <div className='text-[12vw] uppercase leading-'>The spark for</div>
-      <div>allthings</div>
-      <div>creative</div>
+    <div className='font-[font2] text-white pt-5 text-center'>
+      <div className='text-[9.5vw] uppercase leading-[9.5vw]  justify-center flex items-center'>The spark for</div>
+      <div className='text-[9.5vw] uppercase leading-[9.5vw] justify-center flex items-center'>all <div className='h-[8vw] rounded-full overflow-hidden'><Video></Video></div> things</div>
+      <div className='text-[9.5vw] uppercase leading-[9.5vw]  justify-center flex items-center'>creative</div>
     </div>
   );
 };
